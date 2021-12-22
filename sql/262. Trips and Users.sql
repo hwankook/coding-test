1187118d2645bcfaf1739f7aeb7152ad2fe3e36b
Hwankook Lee <hwankooklee@gmail.com>
2021-12-22 17:18:09 +0900

    262. Trips and Users (remove redundant subquery)

--- sql/262. Trips and Users.sql
+++ sql/262. Trips and Users.sql
@@ -1,12 +1,8 @@
-select a.request_at                                                                    as 'Day'
+select request_at                                                                      as 'Day'
      , round(sum(case when status = 'completed' then 0 else 1 end) / count(status), 2) as 'Cancellation Rate'
-from (
-       select a.request_at
-            , a.status
-         from Trips a
-        where client_id = (select users_id from Users where users_id = client_id and banned = 'No')
-          and driver_id = (select users_id from Users where users_id = driver_id and banned = 'No')
-     ) a
- where a.request_at between '2013-10-01' and '2013-10-03'
- group by a.request_at
+  from Trips
+ where client_id = (select users_id from Users where users_id = client_id and banned = 'No')
+   and driver_id = (select users_id from Users where users_id = driver_id and banned = 'No')
+   and request_at between '2013-10-01' and '2013-10-03'
+ group by request_at
 ;
